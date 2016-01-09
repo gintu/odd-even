e67a6942c37f3abd32c9c8272feea87c9ac51b3f
gintu tom <gintutom@gmail.com>
2016-01-10 00:28:24 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,86 @@
-#include<iostream.h>
-void main()
+#include <iostream.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<conio.h>
+class ooe
 {
-printf("hello world");
+    int a,b,c,d,e,f,g,e1,e2,c2;
+    char Z;
+     void bat () ;
+     void bowl() ;
+    pubilc:
+     void entry();
+     void result();
+    ooe()
+    {
+        g=0; a=0;
+        c=0; e=0;
+        f=0;
+    }
+    };
+    void ooe::entry()
+    {
+        lab1:
+        cout<<"/n/n enter a number between 1&6";
+        cin>>e1;
+        if ((e1<1)||(e1>6))
+
+    }
+{
+    cout<<"/a /n invalid entry";
+    goto lab1:
+}
+e2=(rand()%6)+1;
+cout<<"/n The computer has entered"<<e2;
+lab2:
+cout<<"/n odd or even(o/e)";
+cin>>z;
+if ((z!='e')||(z!='o'))
+{
+    cout<<"/a/ninvalid entry!/nthe user is reqested to enter either 'e'or'o'";
+goto lab2;
+
+}
+c1=(e1+e2)%2;
+if((c1==o)&&(z=='e'))
+bat();
+if((c==1)&& (z=='e'))
+bowl();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+void ooe::bat()
+{
+    cout<<"/n /n now you are batting";
+    for(int i=o;i<io;i__)
+    {
+        lab3:
+        cout<<"/n/n you:";
+        cin>>a;
+        if(a>6)
+        {
+            cout <<"/n/n Invalid entry
+            a number between 1&6";
+            goto lab3:
+
+        }
+b=(rand() %6)+1;
+cout<<"n computer:"<<b;
+if (a==b)
+{
+    cout <<"n/a youare out!";
+    cout <<"/n your total scoreis_"
+    <<c<<"-runs.";
+    if (g==0)
 }
